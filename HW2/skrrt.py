dc9bcfd462e9f3c8f3f9617091b193c4dfd7ea13
mcdermatt <mcdermatt@gmail.com>
2020-10-02 13:14:37 -0400

    working on car sprite

--- HW2/skrrt.py
+++ HW2/skrrt.py
@@ -37,29 +37,33 @@ class road:
 		self.states = np.zeros([mapSize,mapSize,5])
 
 		#get args of points on road
-		self.onRoad = np.argwhere(self.gw == 0) * 1000/ self.mapSize
+		self.onRoad = np.argwhere(self.gw == 0) 
 
 		#get args of points not on road
-		self.offRoad = np.argwhere(self.gw == 255) * 1000/ self.mapSize
-		#set negative reward for points off of road
-		self.states[self.offRoad[0],self.offRoad[1],:] = -5
+		self.offRoad = np.argwhere(self.gw == 255)
+		#set large negative reward for points off of road
+		self.states[self.offRoad[:,0],self.offRoad[:,1],:] = -50
 
 		#get args of start points
-		self.onStart = np.argwhere((self.gw < 127) & (self.gw > 0)) * 1000/ self.mapSize
+		self.onStart = np.argwhere((self.gw < 127) & (self.gw > 0)) 
 
 		#get args of end points
-		self.onFinish = np.argwhere((self.gw >= 127) & (self.gw < 255)) * 1000/ self.mapSize
-
-	def draw_map(self):
+		self.onFinish = np.argwhere((self.gw >= 127) & (self.gw < 255)) 
 
 		#draw base map image
 		plt.imshow(self.map, cmap = 'gray', interpolation = 'bicubic')
+	
+
+	def draw_map(self):
+
+		
 
 		#draw grid world
-		plt.plot(self.onRoad[:,1],self.onRoad[:,0],'b.')
-		plt.plot(self.offRoad[:,1],self.offRoad[:,0],'r.')
-		plt.plot(self.onStart[:,1],self.onStart[:,0],'g.')		
-		plt.plot(self.onFinish[:,1],self.onFinish[:,0],'k.')
+		# plt.plot(self.onRoad[:,1]* 1000/ self.mapSize,self.onRoad[:,0]* 1000/ self.mapSize,'b.')
+		# plt.plot(self.offRoad[:,1]* 1000/ self.mapSize,self.offRoad[:,0]* 1000/ self.mapSize,'r.')
+		# plt.plot(self.onStart[:,1]* 1000/ self.mapSize,self.onStart[:,0]* 1000/ self.mapSize,'g.')		
+		# plt.plot(self.onFinish[:,1]* 1000/ self.mapSize,self.onFinish[:,0]* 1000/ self.mapSize,'k.')
+		pass
 
 	def evaluation(self):
 		'''evaluation step of policy improvement'''
@@ -73,7 +77,7 @@ class road:
 
 	def update(self):
 		self.draw_map()
-		plt.pause(5)
+		plt.pause(0.1)
 		plt.draw()
 
 if __name__ == "__main__":
@@ -81,4 +85,23 @@ if __name__ == "__main__":
 	mapFile = "track1.png"
 	mapSize = 30
 	map = road(mapFile, mapSize)
-	map.update()
+	
+	#test moving car
+	#start at random point in atStart
+	print(map.onStart)
+	
+
+	runLen = 15
+	step = 0
+	while step < runLen:
+
+		pos = map.onStart[np.random.randint(0,len(map.onStart))]
+		car, = plt.plot(pos[1] * 1000/ mapSize, pos[0] * 1000 / mapSize,'bo')
+		
+		map.update()
+		car.remove()
+		step += 1
+	
+
+	np.savetxt('states.txt',map.states[:,:,0],fmt='%.0e')
+	# print(map.states)
